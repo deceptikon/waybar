86c01d7a1df744d1236a1fba3e7a415914b85fb4
Lexx.KG <lexx.kg@gmail.com>
2026-06-16 18:27:32 +0600

    split styles, bar adjustment2

--- scripts/waybar-start.sh
+++ scripts/waybar-start.sh
@@ -5,4 +5,9 @@ pkill -x waybar 2>/dev/null || true
 
 waybar &>> /tmp/waybar.log &
 
+<<<<<<< HEAD
+=======
+sleep 1
+
+>>>>>>> a4e4e84 (split styles, bar adjustment)
 waybar -c ~/.config/waybar/config-vertical -s ~/.config/waybar/style-new.css &>> /tmp/waybar-vertical.log &

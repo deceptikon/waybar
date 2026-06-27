7a87126be78ce8c4ad6f0abe750e4a84da97b02b
Lexx <lexx.kg@gmail.com>
2026-06-27 23:05:49 +0600

    bt menu

--- scripts/utils/bt-toggle.sh
+++ scripts/utils/bt-toggle.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-if yad --title="Bluetooth" --text="Toggle Bluetooth?" --button="Yes:0" --button="No:1" --mouse --width=200 --borders=20 --undecorated --on-top; then
-    rfkill toggle bluetooth
-fi
+rfkill toggle bluetooth
